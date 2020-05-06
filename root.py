b54c51b5426b223f1c43f059086e6552c083f355
Edwin Puertas <edwin.puertas@javeriana.edu.co>
2020-05-05 22:06:49 -0500

    Initial Version

---
+++ root.py
@@ -0,0 +1,5 @@
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_INPUT = ROOT_DIR + os.sep + 'data'+ os.sep + 'input' + os.sep
+ROOT_OUTPUT = ROOT_DIR + os.sep + 'data'+ os.sep + 'output' + os.sep
