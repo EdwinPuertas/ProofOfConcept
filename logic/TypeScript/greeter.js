1dd8fb6597410f711f89b5bcf4997447eff8577e
Edwin Puertas <edwin.puertas@javeriana.edu.co>
2020-05-05 22:04:11 -0500

    Initial Version

---
+++ logic/TypeScript/greeter.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Edwin Puertas on 30/01/2017.
+ */
+function greeter(person) {
+    return "Hello, " + person;
+}
+var user = "Jane User";
+document.body.innerHTML = greeter(user);
---
+++ logic/TypeScript/greeter.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by Edwin Puertas on 30/01/2017.
+ */
+function greeter(person) {
+    return "Hello, " + person;
+}
+
+var user = "Jane User";
+
+document.body.innerHTML = greeter(user);
